5d072600a493e6bfade45de13ac1a9d16449a2a6
shalinig99 <shalini.g@neurealm.com>
2026-06-30 21:30:25 +0530

    Delete src/vite.config.js

--- src/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: "/e-plantShopping/", // Sets base path to match your repository name
-  plugins: [react()],
-});
